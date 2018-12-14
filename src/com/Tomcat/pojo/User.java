9ca3754fa33dc6467c10d9a965ce8758a7b2ed01
GS0415 <739864589@qq.com>
2018-12-14 17:56:53 +0800

    test2

---
+++ src/com/Tomcat/pojo/User.java
@@ -0,0 +1,50 @@
+package com.Tomcat.pojo;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String tel;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", tel='" + tel + '\'' +
+                '}';
+    }
+}
